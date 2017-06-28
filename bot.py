d17b39571c5a4870ae4742e3e3fa8c18838afe87
krpromer <ammyiik@gmail.com>
2017-06-28 23:58:58 +0900

    new version

--- bot.py
+++ bot.py
@@ -1,17 +1,40 @@
-﻿#import os
-import ConfigParser
+﻿import ConfigParser
 import subprocess
+import requests
+import json
+import time
 
-p = subprocess.Popen('git pull | find "Already"', shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-for line in p.stdout.readlines():
-    print(line)
+TOKEN = "439659276:AAFoPRUbHjsLOmEtN5aDieR1HjU_VojBmUY"
+CHAT_ID = "64750298"
+POLO_URL = "http://poloniex.com/public?command=returnTicker"
+BOT_URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
-#mFile = open(mPath,'r')
-#lines = mFile.readlines()
-#for line in lines:
-#    print line.split('=')[0],
-#mFile.close()
+BTC_LTC = 0
+PRICE = 0
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+def send_message(text, chat_id):
+    url = BOT_URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
+
+p = subprocess.Popen('git pull', shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
 
 mConfig = ConfigParser.ConfigParser()
 mConfig.read('config.ini')
-print mConfig.get('settings', 'version')
+print 'Version', mConfig.getint('INFO', 'version')
+BTC_LTC = mConfig.getint('COIN', 'BTC_LTC')
+
+coinInfo = json.loads(get_url(POLO_URL))
+
+if BTC_LTC == 1:
+    #print 'BTC_LTC :', coinInfo['BTC_LTC']['last']
+    PRICE = float(coinInfo['BTC_LTC']['last'])
+
+for idx in range(1, 10000):
+    send_message(PRICE, CHAT_ID)
+    time.sleep(10)
+
