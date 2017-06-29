7a1813421af832a9bf002feccca134586b89f148
daehui <ammyiik@gmail.com>
2017-06-30 01:52:54 +0900

    no

--- bot.py
+++ bot.py
@@ -6,11 +6,16 @@ import time
 
 TOKEN = "439659276:AAFoPRUbHjsLOmEtN5aDieR1HjU_VojBmUY"
 CHAT_ID = "64750298"
-POLO_URL = "http://poloniex.com/public?command=returnTicker"
+POLONIEX_URL = "http://poloniex.com/public?command=returnTicker"
+BITTREX_URL = "https://bittrex.com/api/v1.1/public/getticker?market=bt"
 BOT_URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 BTC_LTC = 0
-PRICE = 0
+BTC_WAVES = 0
+PRICE_LTC = 0.000000001
+PRICE_WAVES = 0.000000001
+TEMP = 0.000000001
+run = 1
 
 def get_url(url):
     response = requests.get(url)
@@ -21,20 +26,31 @@ def send_message(text, chat_id):
     url = BOT_URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
-p = subprocess.Popen('git pull', shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+def diff(a, b):
+	if a > b:
+		return a/b
+	else:
+		return b/a
 
-mConfig = ConfigParser.ConfigParser()
-mConfig.read('config.ini')
-print 'Version', mConfig.getint('INFO', 'version')
-BTC_LTC = mConfig.getint('COIN', 'BTC_LTC')
+while run:
+	p = subprocess.Popen('git pull', shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
 
-coinInfo = json.loads(get_url(POLO_URL))
+	mConfig = ConfigParser.ConfigParser()
+	mConfig.read('config.ini')
+#print 'Version', mConfig.getint('INFO', 'version')
+	BTC_LTC = mConfig.getint('COIN', 'BTC_LTC')
+	BTC_WAVES = mConfig.getint('COIN', 'BTC_WAVES')
+	run = mConfig.getint('INFO', 'run')
+	coinInfo = json.loads(get_url(POLONIEX_URL))
 
-if BTC_LTC == 1:
-    #print 'BTC_LTC :', coinInfo['BTC_LTC']['last']
-    PRICE = float(coinInfo['BTC_LTC']['last'])
+	if BTC_LTC == 1:
+		#print 'BTC_LTC :', coinInfo['BTC_LTC']['last']
+		TEMP = float(coinInfo['BTC_LTC']['last'])
+		CH = diff(PRICE_LTC, TEMP)
+		if CH > 1.05:
+			PRICE_LTC = TEMP
+			send_message('LTC', CHAT_ID)
+			send_message(PRICE_LTC, CHAT_ID)
 
-for idx in range(1, 10000):
-    send_message(PRICE, CHAT_ID)
-    time.sleep(10)
+	time.sleep(30)
 
