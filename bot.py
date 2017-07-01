41d980c4b274bc0a358451312e9f0b0b495a688b
krpromer <ammyiik@gmail.com>
2017-07-01 23:53:15 +0900

    mm

--- bot.py
+++ bot.py
@@ -15,10 +15,10 @@ BOT_URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 BTC_LTC = 0
 BTC_WAVES = 0
 BTC_SNM = 0
-PRICE_LTC = 0.000000001
-PRICE_WAVES = 0.000000001
-PRICE_SNM = 0.000000001
-TEMP = 0.000000001
+PRICE_LTC = 0.00000001
+PRICE_WAVES = 0.00000001
+PRICE_SNM = 0.00000001
+TEMP = 0.00000001
 run = 1
 
 def get_url(url):
@@ -46,37 +46,55 @@ while run:
 	BTC_WAVES = mConfig.getint('COIN', 'BTC_WAVES')
 	BTC_SNM = mConfig.getint('COIN', 'BTC_SNM')
 	run = mConfig.getint('INFO', 'run')
-	coinInfo = json.loads(get_url(POLONIEX_URL))
 
 	if BTC_LTC == 1:
-		#print 'BTC_LTC :', coinInfo['BTC_LTC']['last']
-		TEMP = float(coinInfo['BTC_LTC']['last'])
-		CH = diff(PRICE_LTC, TEMP)
-		print 'price=',PRICE_LTC,' now=',TEMP,' debug ltc = ', CH
-		if CH > 1.05:
-			PRICE_LTC = TEMP
-			MSG = "LTC =", TEMP
-			send_message(MSG, CHAT_ID)
+		PRICE_LTC =  mConfig.getfloat('PRICE', 'BTC_LTC')
+		content = get_url(POLONIEX_URL)
+		if content == "":
+			print 'POLO ERROR'
+		else:
+			coinInfo = json.loads(content)
+			TEMP = float(coinInfo['BTC_LTC']['last'])
+			CH = diff(PRICE_LTC, TEMP)
+			print 'price=',PRICE_LTC,' now=',TEMP,' debug ltc = ', CH
+			if CH > 1.05:
+				mConfig.set('PRICE','BTC_LTC',TEMP)
+				MSG = "LTC =", TEMP
+				send_message(MSG, CHAT_ID)
 
 	if BTC_WAVES == 1:
-		coinInfo = json.loads(get_url(BITTREX_URL))
-		TEMP = float(coinInfo['result']['Last'])
-		CH = diff(PRICE_WAVES, TEMP)
-		print 'price=',PRICE_WAVES,' now=',TEMP,' debug waves = ', CH
-		if CH > 1.05:
-			PRICE_WAVES = TEMP
-			MSG = "WAVES =", TEMP
-			send_message(MSG, CHAT_ID)
+		PRICE_WAVES =  mConfig.getfloat('PRICE', 'BTC_WAVES')
+		content = get_url(BITTREX_URL)
+		if content == "":
+			print 'BITTREX ERROR'
+		else:
+			coinInfo = json.loads(content)
+			TEMP = float(coinInfo['result']['Last'])
+			CH = diff(PRICE_WAVES, TEMP)
+			print 'price=',PRICE_WAVES,' now=',TEMP,' debug waves = ', CH
+			if CH > 1.05:
+				mConfig.set('PRICE','BTC_WAVES',TEMP)
+				MSG = "WAVES =", TEMP
+				send_message(MSG, CHAT_ID)
 
 	if BTC_SNM == 1:
-		coinInfo = json.loads(get_url(LIQUI_URL))
-		TEMP = float(coinInfo['snm_btc']['last'])
-		CH = diff(PRICE_SNM, TEMP)
-		print 'price=',"{0:.8f}".format(PRICE_SNM),' now=',"{0:.8f}".format(TEMP),' debug snm = ',"{0:.8f}".format(CH)
-		if CH > 1.05:
-			PRICE_SNM = TEMP
-			MSG = "SNM =", "{0:.8f}".format(TEMP)
-			send_message(MSG, CHAT_ID)
+		PRICE_SNM =  mConfig.getfloat('PRICE', 'BTC_SNM')
+		content = get_url(LIQUI_URL)
+		if content == "":
+			print 'LIQUI ERRROR'
+		else:
+			coinInfo = json.loads(content)
+			TEMP = float(coinInfo['snm_btc']['last'])
+			CH = diff(PRICE_SNM, TEMP)
+			print 'price=',"{0:.8f}".format(PRICE_SNM),' now=',"{0:.8f}".format(TEMP),' debug snm = ',"{0:.8f}".format(CH)
+			if CH > 1.05:
+				mConfig.set('PRICE','BTC_SNM',TEMP)
+				MSG = "SNM =", "{0:.8f}".format(TEMP)
+				send_message(MSG, CHAT_ID)
+	
+	mConfigFile = open('config.ini', 'w')
+	mConfig.write(mConfigFile)
+	mConfigFile.close()
 	print time.strftime("%y/%m/%d %H:%M:%S", time.localtime())
 	time.sleep(30)
 
